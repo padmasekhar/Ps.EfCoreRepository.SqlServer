09ff0ee8c211c88fab8163c90f77e4dce60ce5a3
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-07-15 16:08:53 +0530

    Add project files.

---
+++ DependencyInjection/RepositoryServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ps.EfCoreRepository.SqlServer.DependencyInjection
+{
+    public static class RepositoryServices
+    {
+        public static void AddEfCoreRepository<T>(this IServiceCollection services, IConfiguration config, string configKeyForConnStr = "AppDbConnection") where T : DbContext
+        {
+            services.AddScoped<DbContext, T>();
+            services.AddScoped<IRepository, Repository>();
+            services.AddSqlServerDatabase<T>(config, configKeyForConnStr);
+        }
+        private static IServiceCollection AddSqlServerDatabase<T>(this IServiceCollection services, IConfiguration config, string configKeyForConnStr = "AppDbConnection") where T : DbContext
+        {
+            string connStr = config.GetConnectionString(configKeyForConnStr);
+            services.AddDbContextPool<T>(options => options.UseSqlServer(connStr));
+            return services;
+        }
+    }
+}
